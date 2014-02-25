8a8aba3b0ce74c05b9de4bc97c46f7cc328f47ce
viraldim <viraldim@gmail.com>
2014-02-26 00:23:55 +0200

    Added Enemy movement
    Very very very bad

---
+++ Assets/Scripts/Spider.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spider : MonoBehaviour
+{
+    public Transform target;
+    public int rotationSpeed = 2;
+    public int speed = 5;
+
+    private Transform myTransform;
+
+    Animator animator;
+
+    void Awake()
+    {
+        myTransform = transform;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        GameObject goTo = GameObject.FindGameObjectWithTag("Player");
+        target = goTo.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(target.position, myTransform.position, Color.red);
+
+        //Look at target
+        myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+            Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+
+        //move towards target
+        myTransform.position += myTransform.forward * speed * Time.deltaTime;
+    }
+}
