47843d37b98a1d452ea61920fd8404f13c68eb5e
qwerty <dobromir.v.zlatkov@gmail.com>
2014-02-26 00:58:18 +0200

    some coins

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Player") {
+			Destroy(gameObject);
+		}
+	}
+}
