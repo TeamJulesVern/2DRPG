65621052a5ea4eaea9060f331a7795cf6c135640
PlYovchev <plt3ch@gmail.com>
2014-02-26 00:04:28 +0200

    CameraFix + Side walls

--- Assets/Scripts/Camera2D.cs
+++ Assets/Scripts/Camera2D.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 
 public class Camera2D : MonoBehaviour {
-
+	
 	public Transform player;
-
+	
 	public float smoothRate = 0.5f;
-
+	
 	private Transform thisTransform;
 	private Vector2 velocity;
 	// Use this for initialization
@@ -18,10 +18,21 @@ public class Camera2D : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector2 newPos2D = Vector2.zero;
-
-		newPos2D.x = Mathf.SmoothDamp (thisTransform.position.x, player.position.x, ref velocity.x, smoothRate);
-		newPos2D.y = Mathf.SmoothDamp (thisTransform.position.y, player.position.y, ref velocity.y, smoothRate);
-
+		if ((player.position.x < 3.90f || player.position.x > 35.08f) && (player.position.y > 3.30f || player.position.y < -41.61f)) {
+			newPos2D.x = Mathf.SmoothDamp (thisTransform.position.x, player.position.x<3.90f ? 3.90f : 35.08f, ref velocity.x, smoothRate);
+			newPos2D.y = Mathf.SmoothDamp (thisTransform.position.y, player.position.y > 3.30f ? 3.30f : -41.61f, ref velocity.y, smoothRate);
+		} else if (player.position.y > 3.30f || player.position.y < -41.61f) {
+			newPos2D.x = Mathf.SmoothDamp (thisTransform.position.x, player.position.x, ref velocity.x, smoothRate);
+			newPos2D.y = Mathf.SmoothDamp (thisTransform.position.y, player.position.y > 3.30f ? 3.30f : -41.61f, ref velocity.y, smoothRate);
+		} else if ((player.position.x < 3.90f || player.position.x > 35.08f)) {
+			newPos2D.x = Mathf.SmoothDamp (thisTransform.position.x, player.position.x<3.90f ? 3.90f : 35.08f, ref velocity.x, smoothRate);
+			newPos2D.y = Mathf.SmoothDamp (thisTransform.position.y, player.position.y, ref velocity.y, smoothRate);		
+		} else {
+			newPos2D.x = Mathf.SmoothDamp (thisTransform.position.x, player.position.x, ref velocity.x, smoothRate);
+			newPos2D.y = Mathf.SmoothDamp (thisTransform.position.y, player.position.y, ref velocity.y, smoothRate);
+		}
+		
+		
 		Vector3 newPos = new Vector3 (newPos2D.x, newPos2D.y, transform.position.z);
 		transform.position = Vector3.Slerp (transform.position, newPos, Time.time);
 	}
