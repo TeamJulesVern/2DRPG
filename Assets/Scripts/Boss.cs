aa543fa7fedad62187dd9d0c8005cfa3300572aa
qwerty <dobromir.v.zlatkov@gmail.com>
2014-02-26 18:54:25 +0200

    Controlers and Animations For Bath Boss And Orc

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss : MonoBehaviour
+{
+	public GameManager gameManager;
+	private Transform player;
+	
+	Animator animator;
+	
+	int spiderXp = 10;
+	int spiderHealth = 5;
+	int spiderDmg = 2;
+	float spiderAttackSpeed = 0.3f;
+	float coolDown;
+	float spiderSpeed = 3;
+	
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		//rotates towards player
+		transform.LookAt(player.position);
+		transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+
+		//move towards the player and attacks
+		if (Vector3.Distance(transform.position, player.position) <= 0.5)
+		{
+			animator.SetTrigger ("BossAttack");
+			if (Time.time >= coolDown)
+			{
+
+				gameManager.SendMessage("PlayerDamaged", spiderDmg, SendMessageOptions.DontRequireReceiver);
+				coolDown = Time.time + spiderAttackSpeed;
+			}
+		}
+		else if (Vector3.Distance(transform.position, player.position) <= 4)
+		{           
+			transform.Translate(new Vector3(spiderSpeed * Time.deltaTime, 0, 0));
+			animator.SetTrigger ("BossWalk");
+		}
+		
+	}
+	
+	//Enemy taking dmg
+	void EnemyDamaged(int damage)
+	{
+		if (spiderHealth > 0)
+		{
+			spiderHealth -= damage;
+		}
+		if (spiderHealth <= 0)
+		{
+			spiderHealth = 0;
+			Destroy(gameObject);
+			gameManager.curEXP += spiderXp;
+		}
+	}
+}
---
+++ Assets/Scripts/Orc.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orc : MonoBehaviour 
+{
+	public GameManager gameManager;
+	private Transform player;
+	
+	Animator animator;
+	
+	int batXp = 5;
+	int batHealth = 2;
+	int batDamage = 1;
+	float batAttackSpeed = 0.6f;
+	float coolDown;
+	float batSpeed = 2;
+	
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		//rotates towards player
+		transform.LookAt(player.position);
+		transform.Rotate(new Vector3(0, -90, 0), Space.Self);     
+		
+		//move towards the player and attacks
+		if (Vector3.Distance(transform.position,player.position)<=0.5)
+		{
+
+			if (Time.time>=coolDown)
+			{
+				gameManager.SendMessage("PlayerDamaged", batDamage, SendMessageOptions.DontRequireReceiver);
+				coolDown = Time.time + batAttackSpeed;
+			}
+			
+		}
+		else if (Vector3.Distance(transform.position,player.position)<=3)
+		{
+			transform.Translate(new Vector3(batSpeed*Time.deltaTime,0,0));
+		}
+		
+	}
+	
+	//Enemy taking dmg
+	void EnemyDamaged(int damage)
+	{
+		if (batHealth > 0)
+		{
+			batHealth -= damage;
+		}
+		if (batHealth <= 0)
+		{
+			batHealth = 0;
+			Destroy(gameObject);
+			gameManager.curEXP += batXp;
+		}
+	}
+}
